97f768b3646bdb9827eca842bd55ec598b20d7de
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-11-08 18:28:29 +0530

    demo

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+
+namespace ReflectionDemo1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = 10;
+            Type t = a.GetType();
+            Console.WriteLine(t);
+
+            Type type = typeof(int);
+            Console.WriteLine(type.Assembly);
+
+            Add add = new Add();
+            
+
+            ConstructorInfo[] constructorInfos = t.GetConstructors(BindingFlags.Public | BindingFlags.Instance);
+
+            foreach(ConstructorInfo constructorInfo in constructorInfos)
+            {
+            Console.WriteLine(constructorInfo);
+            }
+
+            MethodInfo[] methodInfos = t.GetMethods(BindingFlags.Public | BindingFlags.Instance);
+
+            foreach(MethodInfo methodInfo in methodInfos)
+            {
+            Console.WriteLine(methodInfos);
+            }
+        
+        }
+    }
+}
