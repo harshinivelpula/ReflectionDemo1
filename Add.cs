2ccc06df25a268bfb2c7f4d8ecb1d43bd6df0549
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-11-08 18:29:27 +0530

    methods

---
+++ Add.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionDemo1
+{
+    internal class Add
+    {
+        public Add()
+        {
+
+        }
+        public Add(int a)
+        {
+
+        }
+        public Add(int a, int b)
+        {
+
+        }
+
+    }
+}
